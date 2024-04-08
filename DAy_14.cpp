f1b33772b39041186810b3d870679aa7386f5fce
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2024-04-08 21:26:59 +0530

    Add files via upload

---
+++ DAy_14.cpp
@@ -0,0 +1,67 @@
+/*
+Name - Himanshu Pokhriyal
+DAte - 8 April , 2024
+Version - C++ 17
+
+
+
+Question -1 Good Investment or Not
+Link --https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INVESTMENT
+
+Apprach & Solution:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    if(x>=2*y){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	    
+	}
+
+}
+
+Time Complexity - O(t)
+Space Complexity - O(1)
+
+
+Question -2 Final Population
+Link - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/POPULATION
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y,z;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y>>z;
+	    cout<<(x-y+z)<<endl;
+	    
+	}
+
+}
+
+Time Complexity - O(t)
+Space Complexity - O(1)
+
+
+
+
+
+
+
+
+
+
+*/
