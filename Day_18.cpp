1270aa27eb087853057689913162b99117bbefc0
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2024-04-12 21:20:46 +0530

    Add files via upload

---
+++ Day_18.cpp
@@ -0,0 +1,51 @@
+/*
+Name -- Himanshu Pokhriyal
+Date -- 12 April , 2024
+version -- C++ 17
+
+Question -1 The Gift
+Link - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CS2023_GIFT
+
+Approach & Solution:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,n,m;
+	cin>>x>>n>>m;
+	if((x+m) >= n) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+
+}
+
+Time & Space Complexity --> O(1)
+
+Question -2 Reach Home
+Link --     https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/REACH_HOME
+
+Approach & Solution:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    if(5*x >= y) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	    
+	}
+
+}
+
+Time - O(t)
+Space - O(1)
+
+
+
+
+
+*/
