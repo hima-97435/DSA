641877b28316c8f237538f51205ab8b872177e91
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2024-04-15 19:59:28 +0530

    Add files via upload

---
+++ day_21.cpp
@@ -0,0 +1,57 @@
+/*
+Name - Himanshu Pokhriyal
+DAte - 15 April , 2024
+version -- C++ 17
+
+Ques -1 Is it hot or cold
+Link - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HOTCOLD
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int C;
+	    cin>>C;
+	    if(C> 20 ) cout<<"HOT"<<endl;
+	    else cout<<"COLD"<<endl;
+	    
+	    
+	}
+
+}
+Time Complexity - O(t)
+Space Complexity -O(1)
+
+
+Que2 - Profit Increment
+Link - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PROINC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int a= x-y;
+	    int x_percent=(10*x)/100;
+	    cout<<(x_percent+x -a )<<endl;
+	}
+
+}
+Time Complexity - O(t)
+Space Complexity -O(1)
+
+
+Que3 
+
+
+
+*/
