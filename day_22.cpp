7aaafd6d830082208728d0383e6cc5b2d03f497c
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2024-04-16 21:48:04 +0530

    Add files via upload

---
+++ day_22.cpp
@@ -0,0 +1,56 @@
+/*
+Name - Himanshu Pokhriyal
+DAte - 16 April , 2024
+version -- C++ 17
+
+Question -1 Ezio and Guards
+Link - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MANIPULATE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	    
+	}
+
+}
+Time Complexity - O(t)
+Space Complexity -O(1)
+
+Question -2 Chef and Masks
+Link - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CMASKS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while( t-- ){
+	    int x, y;
+	    cin>>x>>y;
+	    
+	    if(x*100 < y*10 )  cout<<"Disposable"<<endl;
+	    else cout<<"Cloth"<<endl;
+	    
+	    
+	}
+
+}
+Time Complexity - O(t)
+Space Complexity -O(1)
+
+
+
+
+
+
+*/
