46d46a6c7bcecfcb506c0557fa38e5ab57855d16
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2024-04-18 19:35:18 +0530

    Add files via upload

---
+++ day_24.cpp
@@ -0,0 +1,46 @@
+/*
+Name - Himanshu Pokhiyal
+DAte - 18 April , 2024
+Version -- C++17
+
+
+Ques-1 Increase IQ
+Link - #include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(x+7 > 170 ) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+
+}
+
+Time Complexity -> O(1)
+Space Complexity -> O(1)
+
+Ques-2 Battery Low
+Link - https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BATTERYLOW
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while( t--){
+	    int x;
+	    cin>>x;
+	    if(x<=15) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+
+}
+
+Time Complexity -> O(t)
+Space Complexity -> O(1)
+
+
+*/
