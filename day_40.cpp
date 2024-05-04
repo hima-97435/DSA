68a905818b4a4cb8af0972a8d8353a089d5e3d6d
Himanshu Pokhriyal <145280742+hima-97435@users.noreply.github.com>
2024-05-04 12:00:36 +0530

    Add files via upload

---
+++ day_40.cpp
@@ -0,0 +1,48 @@
+/*
+Name - Himanshu Pokhriyal
+Date - 4 May, 2024
+Version - C++17
+
+Ques-1 Watching Movies at 2x
+Link - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MOVIE2X
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	cout<<(y/2) +(x-y)<<endl;
+
+}
+
+Time complexity -O(1)
+Space Complexity - O(1)
+
+Ques-2 Police and Thief
+Link - https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/POLTHIEF?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while( t-- ){
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    cout<<abs(x-y)<<endl;
+	    
+	    
+	}
+
+}
+Time complexity -O(t)
+Space Complexity - O(1)
+
+
+*/
